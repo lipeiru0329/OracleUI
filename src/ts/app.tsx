9b26fe70acb42c29da2a2d4d70bc2c50a486595f
li peiru <lipeiru0329@gmail.com>
2018-12-09 08:31:57 +0800

    resend op update account when change account

--- src/ts/app.tsx
+++ src/ts/app.tsx
@@ -20,6 +20,7 @@ web3Util.onWeb3AccountUpdate((addr: string, network: number) => {
 	) {
 		store.dispatch(web3Actions.accountUpdate(addr));
 		store.dispatch(web3Actions.networkUpdate(network));
+		wsUtil.connectToRelayer();
 	}
 });
 
